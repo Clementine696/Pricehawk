a539ca3cc7f08aa3fe4471b5f12f8a2fe389687c
Kanate Boonsiri <kanatemask@gmail.com>
2025-12-10 16:01:30 +0700

    Add scraper dependencies to requirements.txt

--- PriceHawkUI/next.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    domains: ['thaiwatsadu.com', 'homepro.co.th', 'globalhouse.co.th', 'dohome.co.th', 'boonthavorn.com'],
-  },
-}
-
-module.exports = nextConfig

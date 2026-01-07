4098af358f5cf2e08a04fe343136bbec59da59b2
Kanate Boonsiri <kanatemask@gmail.com>
2026-01-08 01:24:56 +0700

    add google analyric

--- ui/src/app/layout.tsx
+++ ui/src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from 'next';
+import Script from 'next/script';
 import { AuthProvider } from '@/context/AuthContext';
 import './globals.css';
 
@@ -14,6 +15,20 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-Y4YCTMYX01"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-Y4YCTMYX01');
+          `}
+        </Script>
+      </head>
       <body>
         <AuthProvider>{children}</AuthProvider>
       </body>

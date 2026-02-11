d00f79b607aaa2e02bf5d3da5058a7760b0d444e
Kanate Boonsiri <kanatemask@gmail.com>
2026-02-11 17:10:28 +0700

    hide alert from main

--- ui/src/components/layout/Sidebar.tsx
+++ ui/src/components/layout/Sidebar.tsx
@@ -50,11 +50,11 @@ const menuItems = [
     label: 'Watchlist',
     icon: ListChecks,
   },
-  {
-    href: '/alert',
-    label: 'Price Alerts',
-    icon: Bell,
-  },
+  // {
+  //   href: '/alert',
+  //   label: 'Price Alerts',
+  //   icon: Bell,
+  // },
   // {
   //   href: '/comparison',
   //   label: 'Comparison',
